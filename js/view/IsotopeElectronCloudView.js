b15ac1d05a8eed5d904873a03b9e3e375570f156
jessegreenberg <jesse.greenberg@colorado.edu>
2015-03-03 13:38:56 -0700

    Change some tweak variables so that isotope radii match those of Java sim

--- js/view/IsotopeElectronCloudView.js
+++ js/view/IsotopeElectronCloudView.js
@@ -122,8 +122,8 @@ define( function( require ) {
       var reduceRadiusRange = function( value ) {
         // The following two factors define the way in which an input value is increased or decreased.  These values
         // can be adjusted as needed to make the cloud size appear as desired.
-        var minChangedRadius = 15;
-        var maxChangedRadius = 38;
+        var minChangedRadius = 40;
+        var maxChangedRadius = 55;
 
         var compressionFunction = new LinearFunction( minShellRadius, maxShellRadius, minChangedRadius, maxChangedRadius );
         return compressionFunction( value );
@@ -142,75 +142,3 @@ define( function( require ) {
   } );
 
 } );
-
-// TODO: Keeping it here for reference until the cloud diameter mapping looks good.
-//
-//  /**
-//   * Maps a number of electrons to a diameter in screen coordinates for the
-//   * electron shell.  This mapping function is based on the real size
-//   * relationships between the various atoms, but has some tweakable
-//   * parameters to reduce the range and scale to provide values that
-//   * are usable for our needs on the canvas.
-//   */
-//  private double getElectronShellDiameter( int numElectrons ) {
-//    if ( mapElectronCountToRadius.containsKey( numElectrons ) ) {
-//      return reduceRadiusRange( mapElectronCountToRadius.get( numElectrons ) );
-//    }
-//    else {
-//      if ( numElectrons > MAX_ELECTRONS ) {
-//        System.out.println( getClass().getName() + " - Warning: Atom has more than supported number of electrons, " + numElectrons );
-//      }
-//      return 0;
-//    }
-//  }
-//
-//  // This data structure maps atomic number of atomic radius.  The values
-//  // are the covalent radii, and were taken from a Wikipedia entry entitled
-//  // "Atomic radii of the elements".  Values are in picometers.
-//  private static Map<Integer, Double> mapElectronCountToRadius = new HashMap<Integer, Double>() {{
-//    put( 1, 38d );   // Hydrogen
-//    put( 2, 32d );   // Helium
-//    put( 3, 134d );  // Lithium
-//    put( 4, 90d );   // Beryllium
-//    put( 5, 82d );   // Boron
-//    put( 6, 77d );   // Carbon
-//    put( 7, 75d );   // Nitrogen
-//    put( 8, 73d );   // Oxygen
-//    put( 9, 71d );   // Fluorine
-//    put( 10, 69d );  // Neon
-//  }};
-//
-//  // Determine the min and max radii of the supported atoms.
-//  private static double minShellRadius, maxShellRadius;
-//
-//  static {
-//    minShellRadius = Double.MAX_VALUE;
-//    maxShellRadius = 0;
-//    for ( Double radius : mapElectronCountToRadius.values() ) {
-//      if ( radius > maxShellRadius ) {
-//        maxShellRadius = radius;
-//      }
-//      if ( radius < minShellRadius ) {
-//        minShellRadius = radius;
-//      }
-//    }
-//  }
-//
-//  /**
-//   * This method increases the value of the smaller radius values and
-//   * decreases the value of the larger ones.  This effectively reduces
-//   * the range of radii values used.
-//   * <p/>
-//   * This is a very specialized function for the purposes of this class.
-//   */
-//  private double reduceRadiusRange( double value ) {
-//    // The following two factors define the way in which an input value is
-//    // increased or decreased.  These values can be adjusted as needed
-//    // to make the cloud size appear as desired.
-//    double minChangedRadius = 40;
-//    double maxChangedRadius = 100;
-//
-//    Function.LinearFunction compressionFunction = new Function.LinearFunction( minShellRadius, maxShellRadius, minChangedRadius, maxChangedRadius );
-//    return compressionFunction.evaluate( value );
-//  }
-//}
