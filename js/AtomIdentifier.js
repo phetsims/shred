6eafa63dda7c1dfd241d7847b0a3ec302ce40384
samreid <reids@colorado.edu>
2015-11-06 20:50:41 -0700

    updated copyright info using grunt update-copyright-dates, see  https://github.com/phetsims/chipper/issues/403

--- js/AtomIdentifier.js
+++ js/AtomIdentifier.js
@@ -1,4 +1,4 @@
-// Copyright 2002-2014, University of Colorado Boulder
+// Copyright 2014-2015, University of Colorado Boulder
 
 /**
  * Object that can be used to identify various things about an atom given its configuration, i.e. number of protons,
