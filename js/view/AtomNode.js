phetsims/shred

commit f0056b26715a592d8485de1793376d85327669c1

updated the string names, see https://github.com/phetsims/build-an-atom/issues/106

js/view/AtomNode.js
@@ -26,9 +26,9 @@ define( function( require ) {
   var PhetColorScheme = require( 'SCENERY_PHET/PhetColorScheme' );
 
   // strings
-  var negativeIonString = require( 'string!SHRED/negative.ion' );
-  var neutralAtomString = require( 'string!SHRED/neutral.atom' );
-  var positiveIonString = require( 'string!SHRED/positive.ion' );
+  var minusSignIonString = require( 'string!SHRED/minusSignIon' );
+  var neutralAtomString = require( 'string!SHRED/neutralAtom' );
+  var positiveSignIonString = require( 'string!SHRED/positiveSignIon' );
   var stableString = require( 'string!SHRED/stable' );
   var unstableString = require( 'string!SHRED/unstable' );
 

js/AtomIdentifier.js
@@ -20,24 +20,24 @@ define( function( require ) {
 
   var nameTable = [
     '',
-    require( 'string!SHRED/element.hydrogen.name' ),
-    require( 'string!SHRED/element.helium.name' ),
-    require( 'string!SHRED/element.lithium.name' ),
-    require( 'string!SHRED/element.beryllium.name' ),
-    require( 'string!SHRED/element.boron.name' ),
-    require( 'string!SHRED/element.carbon.name' ),
-    require( 'string!SHRED/element.nitrogen.name' ),
-    require( 'string!SHRED/element.oxygen.name' ),
-    require( 'string!SHRED/element.fluorine.name' ),
-    require( 'string!SHRED/element.neon.name' ),
-    require( 'string!SHRED/element.sodium.name' ),
-    require( 'string!SHRED/element.magnesium.name' ),
-    require( 'string!SHRED/element.aluminum.name' ),
-    require( 'string!SHRED/element.silicon.name' ),
-    require( 'string!SHRED/element.phosphorus.name' ),
-    require( 'string!SHRED/element.sulfur.name' ),
-    require( 'string!SHRED/element.chlorine.name' ),
-    require( 'string!SHRED/element.argon.name' ),
+    require( 'string!SHRED/hydrogen' ),
+    require( 'string!SHRED/helium' ),
+    require( 'string!SHRED/lithium' ),
+    require( 'string!SHRED/beryllium' ),
+    require( 'string!SHRED/boron' ),
+    require( 'string!SHRED/carbon' ),
+    require( 'string!SHRED/nitrogen' ),
+    require( 'string!SHRED/oxygen' ),
+    require( 'string!SHRED/fluorine' ),
+    require( 'string!SHRED/neon' ),
+    require( 'string!SHRED/sodium' ),
+    require( 'string!SHRED/magnesium' ),
+    require( 'string!SHRED/aluminum' ),
+    require( 'string!SHRED/silicon' ),
+    require( 'string!SHRED/phosphorus' ),
+    require( 'string!SHRED/sulfur' ),
+    require( 'string!SHRED/chlorine' ),
+    require( 'string!SHRED/argon' ),
     //TODO Should We just take these out
     'Potassium',
     'Calcium',

js/view/ParticleCountDisplay.js
@@ -21,9 +21,9 @@ define( function( require ) {
   var SharedConstants = require( 'SHRED/SharedConstants' );
 
   // strings
-  var protonsReadoutString = require( 'string!SHRED/protons.readout' );
-  var neutronsReadoutString = require( 'string!SHRED/neutrons.readout' );
-  var electronsReadoutString = require( 'string!SHRED/electrons.readout' );
+  var protonsColonString = require( 'string!SHRED/protonsColon' );
+  var neutronsColonString = require( 'string!SHRED/neutronsColon' );
+  var electronsColonString = require( 'string!SHRED/electronsColon' );
 
   // constants
   var TITLE_MAX_WIDTH_PROPORTION = 1/3;
@@ -42,11 +42,11 @@ define( function( require ) {
     var panelContents = new Node();
 
     var labelOptions = { font: new PhetFont( 14 ) };
-    var protonTitle = new Text( protonsReadoutString, labelOptions );
+    var protonTitle = new Text( protonsColonString, labelOptions );
     panelContents.addChild( protonTitle );
-    var neutronTitle = new Text( neutronsReadoutString, labelOptions );
+    var neutronTitle = new Text( neutronsColonString, labelOptions );
     panelContents.addChild( neutronTitle );
-    var electronTitle = new Text( electronsReadoutString, labelOptions );
+    var electronTitle = new Text( electronsColonString, labelOptions );
     panelContents.addChild( electronTitle );
 
     // Scale the title if more than allowed proportion width

commit 6346738ffc9b4a0f05c1d323b6381b8619e51c0a

Fixing broken string references from https://github.com/phetsims/shred/commit/f0056b26715a592d8485de1793376d85327669c1

js/view/AtomNode.js
@@ -148,11 +148,11 @@ define( function( require ) {
       if ( thisAtomView.atom.protons.length > 0 ) {
         var charge = thisAtomView.atom.getCharge();
         if ( charge < 0 ) {
-          thisAtomView.ionIndicator.text = negativeIonString;
+          thisAtomView.ionIndicator.text = minusSignIonString;
           thisAtomView.ionIndicator.fill = 'blue';
         }
         else if ( charge > 0 ) {
-          thisAtomView.ionIndicator.text = positiveIonString;
+          thisAtomView.ionIndicator.text = positiveSignIonString;
           thisAtomView.ionIndicator.fill = PhetColorScheme.RED_COLORBLIND;
         }
         else {