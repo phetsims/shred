5f50d126b08851cc024282d3180fb2cf42c0ac4d
Aadish Gupta <aadish@digitalgreen.org>
2015-12-07 14:15:19 -0700

    Highlighting selected button

--- js/view/ExpandedPeriodicTableNode.js
+++ js/view/ExpandedPeriodicTableNode.js
@@ -6,6 +6,7 @@ define( function( require ) {
   // modules
   var AtomIdentifier = require( 'SHRED/AtomIdentifier' );
   var inherit = require( 'PHET_CORE/inherit' );
+  var Line = require( 'SCENERY/nodes/Line' );
   var Node = require( 'SCENERY/nodes/Node' );
   var Rectangle = require( 'SCENERY/nodes/Rectangle' );
   var PeriodicTableNode = require( 'SHRED/view/PeriodicTableNode' );
@@ -21,10 +22,13 @@ define( function( require ) {
     [ 0, 1, 3, 4, 5, 6, 7, 8 ]
   ];
 
+  var ENABLED_CELL_COLOR = SharedConstants.DISPLAY_PANEL_BACKGROUND_COLOR;
+  var SELECTED_CELL_COLOR = '#FA8072'; //salmon
   var BUTTON_SIZE = 50;
 
   function ExpandedPeriodicTableNode( numberAtom, interactiveMax ) {
     Node.call( this );
+    var self = this;
     var periodicTableNode = new PeriodicTableNode( numberAtom,
       { interactiveMax: interactiveMax,
         showLabels: false
@@ -32,6 +36,7 @@ define( function( require ) {
     periodicTableNode.scale(0.5);
     this.addChild( periodicTableNode );
 
+    this.cells = [];
     var expandedRowsNode = new Node();
     var elementIndex = 1;
     var rows = 1;
@@ -50,7 +55,7 @@ define( function( require ) {
           listener: function() {
             numberAtom.setSubAtomicParticleCount( atomicNumber, AtomIdentifier.getNumNeutronsInMostCommonIsotope( atomicNumber ), atomicNumber);
           },
-          baseColor: SharedConstants.DISPLAY_PANEL_BACKGROUND_COLOR,
+          baseColor: ENABLED_CELL_COLOR,
           cornerRadius: 0,
           minWidth: BUTTON_SIZE,
           maxWidth: BUTTON_SIZE,
@@ -59,6 +64,7 @@ define( function( require ) {
           font: new PhetFont( 24 )
         });
         button.translation = new Vector2( populatedCellsInRow[ j ] * BUTTON_SIZE, i * BUTTON_SIZE );
+        self.cells.push( button );
         expandedRowsNode.addChild( button );
         j++;
         elementIndex++;
@@ -68,6 +74,32 @@ define( function( require ) {
     expandedRowsNode.top = periodicTableNode.bottom - 30;
     periodicTableNode.centerX = expandedRowsNode.centerX;
     this.addChild(expandedRowsNode);
+
+    var leftConnectingLine = new Line( periodicTableNode.left, periodicTableNode.top,
+      expandedRowsNode.left, expandedRowsNode.top, {
+        stroke: 'Black',
+        lineDash: [ 5, 2 ]
+      });
+    this.addChild( leftConnectingLine );
+
+    var rightConnectingLine = new Line( periodicTableNode.right, periodicTableNode.top,
+      expandedRowsNode.right, expandedRowsNode.top, {
+        stroke: 'Black',
+        lineDash: [ 5, 2 ]
+      });
+    this.addChild( rightConnectingLine );
+
+    // Highlight the cell that corresponds to the atom.
+    var highlightedButton = null;
+    numberAtom.protonCountProperty.link( function( protonCount ) {
+      if ( highlightedButton !== null ) {
+        highlightedButton.baseColor = ENABLED_CELL_COLOR;
+      }
+      if ( protonCount > 0 && protonCount <= self.cells.length ) {
+        highlightedButton = self.cells[ protonCount - 1 ];
+        highlightedButton.baseColor = SELECTED_CELL_COLOR;
+      }
+    } );
   }
 
   return inherit( Node, ExpandedPeriodicTableNode, {
