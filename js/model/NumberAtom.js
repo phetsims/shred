44ddfe6b4c9af4c67ab682df4c97dd1d8e7dabee
samreid <reids@colorado.edu>
2016-07-06 07:05:08 -0600

    Removed tandem validation from NumberAtom.js, see #478

--- js/model/NumberAtom.js
+++ js/model/NumberAtom.js
@@ -29,8 +29,6 @@ define( function( require ) {
       tandem: null
     }, options );
 
-    Tandem.validateOptions( options ); // The tandem is required when brand==='phet-io'
-
     // Call the super constructor.
     // @public
     PropertySet.call( this, {
