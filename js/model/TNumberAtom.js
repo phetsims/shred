0dc67ac864cb65498389d7f217b295deab6d2bf2
zepumph <michael.kauzmann@colorado.edu>
2017-10-11 14:38:12 -0800

    get BAAGameChallenge to create itself on the downstream sim, broken — needs better downstream disposal, https://github.com/phetsims/build-an-atom/issues/156

---
+++ js/model/TNumberAtom.js
@@ -0,0 +1,59 @@
+// Copyright 2017, University of Colorado Boulder
+
+/**
+ * wrapper type for NumberAtom
+ *
+ * @author Michael Kauzamann (PhET Interactive Simulations)
+ * @author John Blanco (PhET Interactive Simulations)
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var assertInstanceOf = require( 'ifphetio!PHET_IO/assertions/assertInstanceOf' );
+  var phetioInherit = require( 'ifphetio!PHET_IO/phetioInherit' );
+  var shred = require( 'SHRED/shred' );
+  var TObject = require( 'ifphetio!PHET_IO/types/TObject' );
+
+  /**
+   * @param {NumberAtom} instance
+   * @param {string} phetioID
+   * @constructor
+   */
+  function TNumberAtom( instance, phetioID ) {
+    TObject.call( this, instance, phetioID );
+    assertInstanceOf( instance, phet.shred.NumberAtom );
+  }
+
+
+  phetioInherit( TObject, 'TNumberAtom', TNumberAtom, {}, {
+
+    documentation: 'A value type that contains quantities of electrons, protons, and neutrons.',
+
+    /**
+     * create a description of the state that isn't automatically handled by the framework (e.g. Property instances)
+     * @param {NumberAtom} instance
+     * @returns {Object}
+     */
+    toStateObject: function( instance ) {
+      return {
+        protonCount: instance.protonCountProperty.get(),
+        electronCount: instance.electronCountProperty.get(),
+        neutronCount: instance.neutronCountProperty.get()
+      };
+    },
+
+    /**
+     * @param {Object} stateObject
+     * @returns {}
+     */
+    fromStateObject: function( stateObject ) { }
+
+  } );
+
+  shred.register( 'TNumberAtom', TNumberAtom );
+
+  return TNumberAtom;
+} );
+
