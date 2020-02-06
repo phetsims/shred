3324950ee90730a861c61e993672a9e3cc8b1b1e
phet-dev <phetadmin@gmail.com>
2020-02-06 03:36:05 -0700

    update copyright dates from daily grunt work

--- js/model/NumberAtom.js
+++ js/model/NumberAtom.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2019, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * Model of an atom that represents the atom as a set of numbers which represent the quantity of the various subatomic
