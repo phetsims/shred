df2082125adcd3bcf9c96fb552237d57f3ee781f
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 15:41:21 -0600

    convert BucketDragListener to https://github.com/phetsims/tasks/issues/1044

--- js/view/BucketDragListener.js
+++ js/view/BucketDragListener.js
@@ -8,57 +8,58 @@
  * @author John Blanco
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import DragListener from '../../../scenery/js/listeners/DragListener.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import shred from '../shred.js';
 
-/**
- * @param {Bucket} bucket
- * @param {BucketFront} bucketView
- * @param {ModelViewTransform2} modelViewTransform
- * @param {Object} [options]
- * @constructor
- */
-function BucketDragListener( bucket, bucketView, modelViewTransform, options ) {
-  options = merge( {
-    tandem: Tandem.REQUIRED
-  }, options );
+class BucketDragListener extends DragListener {
 
-  let activeParticle = null;
+  /**
+   * @param {Bucket} bucket
+   * @param {BucketFront} bucketView
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Object} [options]
+   */
+  constructor( bucket, bucketView, modelViewTransform, options ) {
 
-  const dragListenerOptions = {
-    start: function( event, trail ) {
-      // Note: The following transform works, but it is a bit obscure, and relies on the topology of the scene graph.
-      // JB, SR, and JO discussed potentially better ways to do it. If this code is leveraged, revisit this line for
-      // potential improvement.
-      const positionInModelSpace = modelViewTransform.viewToModelPosition(
-        bucketView.getParents()[ 0 ].globalToLocalPoint( event.pointer.point )
-      );
+    options = merge( {
+      tandem: Tandem.REQUIRED
+    }, options );
 
-      activeParticle = bucket.extractClosestParticle( positionInModelSpace );
-      if ( activeParticle !== null ) {
-        activeParticle.setPositionAndDestination( positionInModelSpace );
-      }
-    },
-    translate: function( translationParams ) {
-      if ( activeParticle !== null ) {
-        activeParticle.setPositionAndDestination( activeParticle.positionProperty.get().plus(
-          modelViewTransform.viewToModelDelta( translationParams.delta ) ) );
-      }
-    },
-    end: function( event ) {
-      if ( activeParticle !== null ) {
-        activeParticle.userControlledProperty.set( false );
-      }
-    },
-    tandem: options.tandem
-  };
+    let activeParticle = null;
 
-  DragListener.call( this, dragListenerOptions );
+    const dragListenerOptions = {
+      start: function( event, trail ) {
+        // Note: The following transform works, but it is a bit obscure, and relies on the topology of the scene graph.
+        // JB, SR, and JO discussed potentially better ways to do it. If this code is leveraged, revisit this line for
+        // potential improvement.
+        const positionInModelSpace = modelViewTransform.viewToModelPosition(
+          bucketView.getParents()[ 0 ].globalToLocalPoint( event.pointer.point )
+        );
+
+        activeParticle = bucket.extractClosestParticle( positionInModelSpace );
+        if ( activeParticle !== null ) {
+          activeParticle.setPositionAndDestination( positionInModelSpace );
+        }
+      },
+      translate: function( translationParams ) {
+        if ( activeParticle !== null ) {
+          activeParticle.setPositionAndDestination( activeParticle.positionProperty.get().plus(
+            modelViewTransform.viewToModelDelta( translationParams.delta ) ) );
+        }
+      },
+      end: function( event ) {
+        if ( activeParticle !== null ) {
+          activeParticle.userControlledProperty.set( false );
+        }
+      },
+      tandem: options.tandem
+    };
+
+    super( dragListenerOptions );
+  }
 }
 
 shred.register( 'BucketDragListener', BucketDragListener );
-inherit( DragListener, BucketDragListener );
 export default BucketDragListener;
