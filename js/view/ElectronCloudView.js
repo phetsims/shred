43da954b93e67a072832d9e7e29d262593fe21c6
jbphet <john.blanco@colorado.edu>
2020-09-25 13:46:51 -0600

    used more conventional constructor, migrated to ES6-style class

--- js/view/ElectronCloudView.js
+++ js/view/ElectronCloudView.js
@@ -7,7 +7,6 @@
  * @author John Blanco
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import SimpleDragHandler from '../../../scenery/js/input/SimpleDragHandler.js';
 import Circle from '../../../scenery/js/nodes/Circle.js';
@@ -16,98 +15,98 @@ import Tandem from '../../../tandem/js/Tandem.js';
 import shred from '../shred.js';
 import ShredConstants from '../ShredConstants.js';
 
-/**
- * @param {ParticleAtom} atom
- * @param {ModelViewTransform2} modelViewTransform
- * @param {Object} [options]
- * @constructor
- */
-function ElectronCloudView( atom, modelViewTransform, options ) {
+// constants
+const DEFAULT_RADIUS = 50; // in pm, chosen as an arbitrary value that is close to the "real" values that are used
 
-  const self = this;
-  options = merge( { tandem: Tandem.REQUIRED }, options );
+class ElectronCloudView extends Circle {
 
-  Circle.call( this, {
-      cursor: 'pointer',
-      fill: 'pink',
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } )
-    }
-  );
+  /**
+   * @param {ParticleAtom} atom
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Object} [options]
+   */
+  constructor( atom, modelViewTransform, options ) {
 
-  // Function that updates the size of the cloud based on the number of electrons.
-  const update = function( numElectrons ) {
-    if ( numElectrons === 0 ) {
-      self.radius = 1E-5; // Arbitrary non-zero value.
-      self.fill = 'transparent';
-    }
-    else {
-      const minRadius = modelViewTransform.modelToViewDeltaX( atom.innerElectronShellRadius ) * 0.5;
-      const maxRadius = modelViewTransform.modelToViewDeltaX( atom.outerElectronShellRadius );
-      const radius = minRadius + ( ( maxRadius - minRadius ) / ShredConstants.MAX_ELECTRONS ) * numElectrons;
-      self.radius = radius;
-      self.fill = new RadialGradient( 0, 0, 0, 0, 0, radius )
-        .addColorStop( 0, 'rgba( 0, 0, 255, 200 )' )
-        .addColorStop( 0.9, 'rgba( 0, 0, 255, 0 )' );
-    }
-  };
-  update( atom.electrons.length );
+    options = merge( { tandem: Tandem.REQUIRED }, options );
 
-  // Update the cloud size as electrons come and go.
-  atom.electrons.lengthProperty.link( update );
-
-  // If the user clicks on the cloud, extract an electron.
-  this.extractedElectron = null; // @private
-  const simpleDragHandler = new SimpleDragHandler( {
-    start: function( event, trail ) {
-
-      // Note: The following transform works, but it is a bit obscure, and relies on the topology of the scene graph.
-      // JB, SR, and JO discussed potentially better ways to do it. If this code is leveraged, revisit this line for
-      // potential improvement.
-      const positionInModelSpace = modelViewTransform.viewToModelPosition(
-        self.getParents()[ 0 ].globalToLocalPoint( event.pointer.point )
-      );
-
-      const electron = atom.extractParticle( 'electron' );
-      if ( electron !== null ) {
-        electron.userControlledProperty.set( true );
-        electron.setPositionAndDestination( positionInModelSpace );
-        self.extractedElectron = electron;
+    super( DEFAULT_RADIUS, {
+        cursor: 'pointer',
+        fill: 'pink',
+        translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } )
       }
-    },
-    translate: function( translationParams ) {
-      if ( self.extractedElectron !== null ) {
-        self.extractedElectron.setPositionAndDestination(
-          self.extractedElectron.positionProperty.get().plus(
-            modelViewTransform.viewToModelDelta( translationParams.delta ) ) );
+    );
+
+    // Function that updates the size of the cloud based on the number of electrons.
+    const update = numElectrons => {
+      if ( numElectrons === 0 ) {
+        this.radius = 1E-5; // Arbitrary non-zero value.
+        this.fill = 'transparent';
       }
-    },
-    end: function( event ) {
-      if ( self.extractedElectron !== null ) {
-        self.extractedElectron.userControlledProperty.set( false );
+      else {
+        const minRadius = modelViewTransform.modelToViewDeltaX( atom.innerElectronShellRadius ) * 0.5;
+        const maxRadius = modelViewTransform.modelToViewDeltaX( atom.outerElectronShellRadius );
+        const radius = minRadius + ( ( maxRadius - minRadius ) / ShredConstants.MAX_ELECTRONS ) * numElectrons;
+        this.radius = radius;
+        this.fill = new RadialGradient( 0, 0, 0, 0, 0, radius )
+          .addColorStop( 0, 'rgba( 0, 0, 255, 200 )' )
+          .addColorStop( 0.9, 'rgba( 0, 0, 255, 0 )' );
       }
-    },
-    tandem: options.tandem.createTandem( 'dragHandler' )
-  } );
-  this.addInputListener( simpleDragHandler );
+    };
+    update( atom.electrons.length );
 
-  // @private called by dispose
-  this.disposeElectronCloudView = function() {
-    atom.electrons.lengthProperty.unlink( update );
-    simpleDragHandler.dispose();
-  };
+    // Update the cloud size as electrons come and go.
+    atom.electrons.lengthProperty.link( update );
 
-  this.mutate( options );
-}
+    // If the user clicks on the cloud, extract an electron.
+    this.extractedElectron = null; // @private
+    const simpleDragHandler = new SimpleDragHandler( {
+      start: event => {
 
-shred.register( 'ElectronCloudView', ElectronCloudView );
+        // Note: The following transform works, but it is a bit obscure, and relies on the topology of the scene graph.
+        // JB, SR, and JO discussed potentially better ways to do it. If this code is leveraged, revisit this line for
+        // potential improvement.
+        const positionInModelSpace = modelViewTransform.viewToModelPosition(
+          this.getParents()[ 0 ].globalToLocalPoint( event.pointer.point )
+        );
 
-inherit( Circle, ElectronCloudView, {
+        const electron = atom.extractParticle( 'electron' );
+        if ( electron !== null ) {
+          electron.userControlledProperty.set( true );
+          electron.setPositionAndDestination( positionInModelSpace );
+          this.extractedElectron = electron;
+        }
+      },
+      translate: translationParams => {
+        if ( this.extractedElectron !== null ) {
+          this.extractedElectron.setPositionAndDestination(
+            this.extractedElectron.positionProperty.get().plus(
+              modelViewTransform.viewToModelDelta( translationParams.delta ) ) );
+        }
+      },
+      end: () => {
+        if ( this.extractedElectron !== null ) {
+          this.extractedElectron.userControlledProperty.set( false );
+        }
+      },
+      tandem: options.tandem.createTandem( 'dragHandler' )
+    } );
+    this.addInputListener( simpleDragHandler );
+
+    // @private called by dispose
+    this.disposeElectronCloudView = () => {
+      atom.electrons.lengthProperty.unlink( update );
+      simpleDragHandler.dispose();
+    };
+
+    this.mutate( options );
+  }
 
   // @public
-  dispose: function() {
+  dispose() {
     this.disposeElectronCloudView();
     Circle.prototype.dispose.call( this );
   }
-} );
+}
 
+shred.register( 'ElectronCloudView', ElectronCloudView );
 export default ElectronCloudView;
