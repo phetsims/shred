eef4ea7b8d42ea5116cd28d82980e0ae4b587f5a
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 15:36:47 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/view/PeriodicTableCell.js
+++ js/view/PeriodicTableCell.js
@@ -6,7 +6,6 @@
  * @author Aadish Gupta
  */
 
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import PhetColorScheme from '../../../scenery-phet/js/PhetColorScheme.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
@@ -22,88 +21,91 @@ import shred from '../shred.js';
 const NOMINAL_CELL_DIMENSION = 25;
 const NOMINAL_FONT_SIZE = 14;
 
-/**
- * @param {number} atomicNumber - Atomic number of atom represented by this cell.
- * @param {NumberAtom} numberAtom - Atom that is set if this cell is selected by the user.
- * @param {Color} cellColor - Color to be used for selected enabled and disabled cell
- * @param {Object} [options]
- * @constructor
- */
-function PeriodicTableCell( atomicNumber, numberAtom, cellColor, options ) {
-  options = merge( {
-    length: 25, //Width and height of cell (cells are square).
-    interactive: false, // Boolean flag that determines whether cell is interactive.
-    showLabels: true,
-    tandem: Tandem.REQUIRED,
-    phetioEventType: EventType.USER
-  }, options );
+class PeriodicTableCell extends Rectangle {
 
-  this.options = options;
+  /**
+   * @param {number} atomicNumber - Atomic number of atom represented by this cell.
+   * @param {NumberAtom} numberAtom - Atom that is set if this cell is selected by the user.
+   * @param {Color} cellColor - Color to be used for selected enabled and disabled cell
+   * @param {Object} [options]
+   */
+  constructor( atomicNumber, numberAtom, cellColor, options ) {
 
-  // @private
-  this.normalFill = options.interactive ? cellColor.enabled : cellColor.disabled;
-  this.highlightedFill = cellColor.selected;
+    options = merge( {
+      length: 25, //Width and height of cell (cells are square).
+      interactive: false, // Boolean flag that determines whether cell is interactive.
+      showLabels: true,
+      tandem: Tandem.REQUIRED,
+      phetioEventType: EventType.USER
+    }, options );
 
-  Rectangle.call( this, 0, 0, options.length, options.length, 0, 0, {
-    stroke: 'black',
-    lineWidth: 1,
-    fill: this.normalFill,
-    cursor: options.interactive ? 'pointer' : null,
-    tandem: options.tandem,
-    phetioType: options.phetioType
-  } ); // Call super constructor.
+    const normalFill = options.interactive ? cellColor.enabled : cellColor.disabled;
 
-  if ( options.showLabels ) {
-    // @private
-    this.label = new Text( AtomIdentifier.getSymbol( atomicNumber ), {
-      font: new PhetFont( NOMINAL_FONT_SIZE * ( options.length / NOMINAL_CELL_DIMENSION ) ),
-      center: this.center,
-      maxWidth: options.length - 5,
-      tandem: options.tandem.createTandem( 'label' )
+    super( 0, 0, options.length, options.length, 0, 0, {
+      stroke: 'black',
+      lineWidth: 1,
+      fill: normalFill,
+      cursor: options.interactive ? 'pointer' : null,
+      tandem: options.tandem,
+      phetioType: options.phetioType
     } );
-    this.addChild( this.label );
-  }
 
-  // If interactive, add a listener to set the atom when this cell is pressed.
-  let buttonListener = null; // scope for disposal
-  if ( options.interactive ) {
-    buttonListener = new FireListener( {
-      tandem: options.tandem.createTandem( 'inputListener' ),
-      fire: function( evt ) {
-        numberAtom.setSubAtomicParticleCount(
+    // @private
+    this.options = options;
+    this.normalFill = normalFill;
+    this.highlightedFill = cellColor.selected;
+
+    if ( options.showLabels ) {
+      // @private
+      this.label = new Text( AtomIdentifier.getSymbol( atomicNumber ), {
+        font: new PhetFont( NOMINAL_FONT_SIZE * ( options.length / NOMINAL_CELL_DIMENSION ) ),
+        center: this.center,
+        maxWidth: options.length - 5,
+        tandem: options.tandem.createTandem( 'label' )
+      } );
+      this.addChild( this.label );
+    }
+
+    // If interactive, add a listener to set the atom when this cell is pressed.
+    let buttonListener = null; // scope for disposal
+    if ( options.interactive ) {
+      buttonListener = new FireListener( {
+        tandem: options.tandem.createTandem( 'inputListener' ),
+        fire: () => numberAtom.setSubAtomicParticleCount(
           atomicNumber,
           AtomIdentifier.getNumNeutronsInMostCommonIsotope( atomicNumber ),
           atomicNumber
-        );
-      }
-    } );
-    this.addInputListener( buttonListener );
+          )
+      } );
+      this.addInputListener( buttonListener );
+    }
+
+    // @private called by dispose
+    this.disposePeriodicTableCell = () => {
+      this.label.dispose();
+      buttonListener && buttonListener.dispose();
+    };
   }
 
-  // @private called by dispose
-  this.disposePeriodicTableCell = function() {
-    this.label.dispose();
-    buttonListener && buttonListener.dispose();
-  };
-}
-
-shred.register( 'PeriodicTableCell', PeriodicTableCell );
-
-inherit( Rectangle, PeriodicTableCell, {
-
-  setHighlighted: function( highLighted ) {
+  // @public
+  setHighlighted( highLighted ) {
     this.fill = highLighted ? this.highlightedFill : this.normalFill;
     this.stroke = highLighted ? PhetColorScheme.RED_COLORBLIND : 'black';
     this.lineWidth = highLighted ? 2 : 1;
     if ( this.options.showLabels ) {
       this.label.fontWeight = highLighted ? 'bold' : 'normal';
     }
-  },
-
-  dispose: function() {
-    this.disposePeriodicTableCell();
-    Rectangle.prototype.dispose.call( this );
   }
-} );
 
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposePeriodicTableCell();
+    super.dispose();
+  }
+}
+
+shred.register( 'PeriodicTableCell', PeriodicTableCell );
 export default PeriodicTableCell;
