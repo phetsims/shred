dfd9162e5aade42f5b2ac9cd84d40a45164b17fe
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 16:04:36 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/view/ParticleView.js
+++ js/view/ParticleView.js
@@ -6,7 +6,6 @@
 
 import Property from '../../../axon/js/Property.js';
 import Bounds2 from '../../../dot/js/Bounds2.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import DragListener from '../../../scenery/js/listeners/DragListener.js';
 import Node from '../../../scenery/js/nodes/Node.js';
@@ -15,7 +14,95 @@ import shred from '../shred.js';
 import IsotopeNode from './IsotopeNode.js';
 import ParticleNode from './ParticleNode.js';
 
-// helper factory function
+class ParticleView extends Node {
+
+  /**
+   * @param {Particle} particle
+   * @param {ModelViewTransform2} modelViewTransform
+   * @param {Object} [options]
+   */
+  constructor( particle, modelViewTransform, options ) {
+
+    options = merge( {
+      dragBounds: Bounds2.EVERYTHING,
+      tandem: Tandem.REQUIRED
+    }, options );
+
+    super();
+
+    this.particle = particle; // @public
+
+    // Add the particle representation.
+    const particleNode = createParticleNode(
+      particle,
+      modelViewTransform,
+      options.tandem.createTandem( 'particleNode' )
+    );
+    this.addChild( particleNode );
+
+    // Listen to the model position and update.
+    const updateParticlePosition = position => {
+      this.translation = modelViewTransform.modelToViewPosition( position );
+    };
+    particle.positionProperty.link( updateParticlePosition );
+
+    // add a drag listener
+    const dragListener = new DragListener( {
+
+      positionProperty: particle.destinationProperty,
+      transform: modelViewTransform,
+      dragBoundsProperty: options.dragBounds ? new Property( options.dragBounds ) : null,
+      tandem: options.tandem.createTandem( 'dragListener' ),
+
+      start: () => {
+        this.particle.userControlledProperty.set( true );
+
+        // if there is an animation in progress, cancel it be setting the destination to the position
+        if ( !particle.positionProperty.get().equals( particle.destinationProperty.get() ) ) {
+          particle.destinationProperty.set( particle.positionProperty.get() );
+        }
+      },
+
+      drag: () => {
+
+        // Because the destination property is what is being set by the drag listener, we need to tell the particle to
+        // go immediately to its destination when a drag occurs.
+        this.particle.moveImmediatelyToDestination();
+      },
+
+      end: () => {
+        this.particle.userControlledProperty.set( false );
+      }
+    } );
+    this.addInputListener( dragListener );
+
+    this.mutate( options );
+
+    // @private called by dispose
+    this.disposeParticleView = function() {
+      particle.positionProperty.unlink( updateParticlePosition );
+      particleNode.dispose();
+      dragListener.dispose();
+    };
+  }
+
+  /**
+   * @public
+   * @override
+   */
+  dispose() {
+    this.disposeParticleView();
+    super.dispose();
+  }
+}
+
+/**
+ * Creates the proper view for a particle.
+ * @param {Particle} particle
+ * @param {ModelViewTransform2} modelViewTransform
+ * @param {Tandem} tandem
+ * @returns {Node}
+ */
 function createParticleNode( particle, modelViewTransform, tandem ) {
   let particleNode;
   if ( particle.type === 'Isotope' ) {
@@ -35,87 +122,5 @@ function createParticleNode( particle, modelViewTransform, tandem ) {
   return particleNode;
 }
 
-/**
- * @param {Particle} particle
- * @param {ModelViewTransform2} modelViewTransform
- * @param {Object} [options]
- * @constructor
- */
-function ParticleView( particle, modelViewTransform, options ) {
-
-  options = merge( {
-    dragBounds: Bounds2.EVERYTHING,
-    tandem: Tandem.REQUIRED
-  }, options );
-
-  Node.call( this ); // Call super constructor.
-  const self = this;
-
-  // Set up fields.
-  this.particle = particle; // @public
-  this.modelViewTransform = modelViewTransform; // @private
-
-  // Add the particle representation.
-  const particleNode = createParticleNode(
-    particle,
-    modelViewTransform,
-    options.tandem.createTandem( 'particleRepresentation' )
-  );
-  this.addChild( particleNode );
-
-  // Listen to the model position and update.
-  const updateParticlePosition = function( position ) {
-    self.translation = self.modelViewTransform.modelToViewPosition( position );
-  };
-  particle.positionProperty.link( updateParticlePosition );
-
-  // add a drag listener
-  const dragListener = new DragListener( {
-
-    positionProperty: particle.destinationProperty,
-    tandem: options.tandem ? options.tandem.createTandem( 'inputListener' ) : null,
-
-    start: function( event, trail ) {
-      self.particle.userControlledProperty.set( true );
-
-      // if there is an animation in progress, cancel it be setting the destination to the position
-      if ( !particle.positionProperty.get().equals( particle.destinationProperty.get() ) ) {
-        particle.destinationProperty.set( particle.positionProperty.get() );
-      }
-    },
-
-    drag: () => {
-
-      // Because the destination property is what is being set by the drag listener, we need to tell the particle to go
-      // immediately to its destination when a drag occurs.
-      self.particle.moveImmediatelyToDestination();
-    },
-
-    end: function( event, trail ) {
-      self.particle.userControlledProperty.set( false );
-    },
-
-    transform: modelViewTransform,
-    dragBoundsProperty: options.dragBounds ? new Property( options.dragBounds ) : null
-  } );
-  this.addInputListener( dragListener );
-  this.mutate( options );
-
-  // @private called by dispose
-  this.disposeParticleView = function() {
-    particle.positionProperty.unlink( updateParticlePosition );
-    particleNode.dispose();
-    dragListener.dispose();
-  };
-}
-
 shred.register( 'ParticleView', ParticleView );
-// Inherit from Node.
-inherit( Node, ParticleView, {
-  dispose: function() {
-    this.disposeParticleView();
-    Node.prototype.dispose.call( this );
-  }
-} );
-
 export default ParticleView;
