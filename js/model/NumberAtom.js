9bc59423700e95bd515bfcf78be91938c0c259af
pixelzoom <cmalley@pixelzoom.com>
2020-10-13 16:19:43 -0600

    convert to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/model/NumberAtom.js
+++ js/model/NumberAtom.js
@@ -10,7 +10,6 @@
 import DerivedProperty from '../../../axon/js/DerivedProperty.js';
 import Emitter from '../../../axon/js/Emitter.js';
 import NumberProperty from '../../../axon/js/NumberProperty.js';
-import inherit from '../../../phet-core/js/inherit.js';
 import merge from '../../../phet-core/js/merge.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import IOType from '../../../tandem/js/types/IOType.js';
@@ -21,108 +20,107 @@ import shred from '../shred.js';
 // constants
 const NumberDerivedProperty = DerivedProperty.DerivedPropertyIO( NumberIO );
 
-/**
- * @param {Object} [options]
- * @constructor
- */
-function NumberAtom( options ) {
+class NumberAtom {
 
-  // Default configuration.
-  options = merge( {
-    protonCount: 0,
-    neutronCount: 0,
-    electronCount: 0,
-    tandem: Tandem.OPTIONAL // Tandem must be supplied when running in PhET-iO
-  }, options );
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
 
-  // @public
-  this.protonCountProperty = new NumberProperty( options.protonCount, {
-    tandem: options.tandem.createTandem( 'protonCountProperty' ),
-    documentation: 'this property is updated by the model and should not be set by users',
-    numberType: 'Integer'
-  } );
-  this.neutronCountProperty = new NumberProperty( options.neutronCount, {
-    tandem: options.tandem.createTandem( 'neutronCountProperty' ),
-    numberType: 'Integer',
-    documentation: 'this property is updated by the model and should not be set by users'
-  } );
-  this.electronCountProperty = new NumberProperty( options.electronCount, {
-    tandem: options.tandem.createTandem( 'electronCountProperty' ),
-    numberType: 'Integer',
-    documentation: 'this property is updated by the model and should not be set by users'
-  } );
+    // Default configuration.
+    options = merge( {
+      protonCount: 0,
+      neutronCount: 0,
+      electronCount: 0,
+      tandem: Tandem.OPTIONAL // Tandem must be supplied when running in PhET-iO
+    }, options );
 
-  this.chargeProperty = new DerivedProperty( [ this.protonCountProperty, this.electronCountProperty ],
-    function( protonCount, electronCount ) {
-      return protonCount - electronCount;
-    }, {
-      tandem: options.tandem.createTandem( 'chargeProperty' ),
+    // @public
+    this.protonCountProperty = new NumberProperty( options.protonCount, {
+      tandem: options.tandem.createTandem( 'protonCountProperty' ),
+      documentation: 'this property is updated by the model and should not be set by users',
+      numberType: 'Integer'
+    } );
+    this.neutronCountProperty = new NumberProperty( options.neutronCount, {
+      tandem: options.tandem.createTandem( 'neutronCountProperty' ),
       numberType: 'Integer',
-      phetioType: NumberDerivedProperty
-    }
-  );
-
-  this.massNumberProperty = new DerivedProperty( [ this.protonCountProperty, this.neutronCountProperty ],
-    function( protonCount, neutronCount ) {
-      return protonCount + neutronCount;
-    }, {
-      tandem: options.tandem.createTandem( 'massNumberProperty' ),
+      documentation: 'this property is updated by the model and should not be set by users'
+    } );
+    this.electronCountProperty = new NumberProperty( options.electronCount, {
+      tandem: options.tandem.createTandem( 'electronCountProperty' ),
       numberType: 'Integer',
-      phetioType: NumberDerivedProperty
-    }
-  );
+      documentation: 'this property is updated by the model and should not be set by users'
+    } );
 
-  this.particleCountProperty = new DerivedProperty( [ this.protonCountProperty, this.neutronCountProperty, this.electronCountProperty ],
-    function( protonCount, neutronCount, electronCount ) {
-      return protonCount + neutronCount + electronCount;
-    }, {
-      tandem: options.tandem.createTandem( 'particleCountProperty' ),
-      numberType: 'Integer',
-      phetioType: NumberDerivedProperty
-    }
-  );
+    this.chargeProperty = new DerivedProperty( [ this.protonCountProperty, this.electronCountProperty ],
+      function( protonCount, electronCount ) {
+        return protonCount - electronCount;
+      }, {
+        tandem: options.tandem.createTandem( 'chargeProperty' ),
+        numberType: 'Integer',
+        phetioType: NumberDerivedProperty
+      }
+    );
 
-  // @public - events emitted by instances of this type
-  this.atomUpdated = new Emitter( {
-    tandem: options.tandem.createTandem( 'atomUpdatedEmitter' )
-  } );
-}
+    this.massNumberProperty = new DerivedProperty( [ this.protonCountProperty, this.neutronCountProperty ],
+      function( protonCount, neutronCount ) {
+        return protonCount + neutronCount;
+      }, {
+        tandem: options.tandem.createTandem( 'massNumberProperty' ),
+        numberType: 'Integer',
+        phetioType: NumberDerivedProperty
+      }
+    );
 
-shred.register( 'NumberAtom', NumberAtom );
+    this.particleCountProperty = new DerivedProperty( [ this.protonCountProperty, this.neutronCountProperty, this.electronCountProperty ],
+      function( protonCount, neutronCount, electronCount ) {
+        return protonCount + neutronCount + electronCount;
+      }, {
+        tandem: options.tandem.createTandem( 'particleCountProperty' ),
+        numberType: 'Integer',
+        phetioType: NumberDerivedProperty
+      }
+    );
 
-inherit( Object, NumberAtom, {
+    // @public - events emitted by instances of this type
+    this.atomUpdated = new Emitter( {
+      tandem: options.tandem.createTandem( 'atomUpdatedEmitter' )
+    } );
+  }
+
+  // @public - ES5 getters for particle counts
+  get protonCount() {
+    return this.protonCountProperty.value;
+  }
+
+  get neutronCount() {
+    return this.neutronCountProperty.value;
+  }
+
+  get electronCount() {
+    return this.electronCountProperty.value;
+  }
 
   /**
    * Compare with other Number Atom
    * @param {NumberAtom|ImmutableAtomConfig} otherAtom
    * @public
    */
-  equals: function( otherAtom ) {
+  equals( otherAtom ) {
     return this.protonCount === otherAtom.protonCount &&
            this.neutronCount === otherAtom.neutronCount &&
            this.electronCount === otherAtom.electronCount;
-  },
+  }
 
   // @public
-  getStandardAtomicMass: function() {
+  getStandardAtomicMass() {
     return AtomIdentifier.getStandardAtomicMass( this.protonCountProperty.get() + this.neutronCountProperty.get() );
-  },
+  }
 
   // @public
-  getIsotopeAtomicMass: function() {
+  getIsotopeAtomicMass() {
     return AtomIdentifier.getIsotopeAtomicMass( this.protonCountProperty.get(), this.neutronCountProperty.get() );
-  },
-
-  // @public - ES5 getters for particle counts
-  get protonCount() {
-    return this.protonCountProperty.value;
-  },
-  get neutronCount() {
-    return this.neutronCountProperty.value;
-  },
-  get electronCount() {
-    return this.electronCountProperty.value;
-  },
+  }
 
   /**
    * @param {number} protonCount
@@ -130,14 +128,17 @@ inherit( Object, NumberAtom, {
    * @param {number} electronCount
    * @public
    */
-  setSubAtomicParticleCount: function( protonCount, neutronCount, electronCount ) {
+  setSubAtomicParticleCount( protonCount, neutronCount, electronCount ) {
     this.protonCountProperty.set( protonCount );
     this.electronCountProperty.set( electronCount );
     this.neutronCountProperty.set( neutronCount );
     this.atomUpdated.emit();
-  },
+  }
 
-  dispose: function() {
+  /**
+   * @public
+   */
+  dispose() {
     this.chargeProperty.dispose();
     this.massNumberProperty.dispose();
     this.particleCountProperty.dispose();
@@ -149,7 +150,7 @@ inherit( Object, NumberAtom, {
 
     this.atomUpdated.dispose();
   }
-} );
+}
 
 NumberAtom.NumberAtomIO = new IOType( 'NumberAtomIO', {
   valueType: NumberAtom,
@@ -162,4 +163,5 @@ NumberAtom.NumberAtomIO = new IOType( 'NumberAtomIO', {
   fromStateObject( stateObject ) { } // TODO: Should this be implemented?
 } );
 
+shred.register( 'NumberAtom', NumberAtom );
 export default NumberAtom;
