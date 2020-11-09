6760e9eb19e25185b3474e6abc4a7629f0783a12
jbphet <john.blanco@colorado.edu>
2020-11-09 16:34:47 -0700

    temporary change to particle appearance for testing high contrast, see https://github.com/phetsims/build-an-atom/issues/217

--- js/view/ParticleNode.js
+++ js/view/ParticleNode.js
@@ -9,15 +9,15 @@ import merge from '../../../phet-core/js/merge.js';
 import PhetColorScheme from '../../../scenery-phet/js/PhetColorScheme.js';
 import Circle from '../../../scenery/js/nodes/Circle.js';
 import Node from '../../../scenery/js/nodes/Node.js';
-import RadialGradient from '../../../scenery/js/util/RadialGradient.js';
+import Color from '../../../scenery/js/util/Color.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import shred from '../shred.js';
 
-// Map of particle type to color
+// Map of particle type to color information
 const PARTICLE_COLORS = {
   proton: PhetColorScheme.RED_COLORBLIND,
-  neutron: 'gray',
-  electron: 'blue'
+  neutron: Color.GRAY,
+  electron: Color.BLUE
 };
 
 class ParticleNode extends Node {
@@ -43,9 +43,9 @@ class ParticleNode extends Node {
 
     // Create the node a circle with a gradient.
     this.addChild( new Circle( radius, {
-      fill: new RadialGradient( -radius * 0.4, -radius * 0.4, 0, -radius * 0.4, -radius * 0.4, radius * 1.6 )
-        .addColorStop( 0, 'white' )
-        .addColorStop( 1, baseColor ),
+      fill: baseColor,
+      stroke: baseColor.colorUtilsDarker( 0.6 ),
+      lineWidth: 2,
       cursor: 'pointer'
     } ) );
   }
