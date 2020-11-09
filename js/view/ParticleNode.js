d53c77d8ee0e741a0ab806d335b232482c095a97
jbphet <john.blanco@colorado.edu>
2020-11-09 16:45:03 -0700

    another temporary change to particle appearance for testing high contrast, see https://github.com/phetsims/build-an-atom/issues/217

--- js/view/ParticleNode.js
+++ js/view/ParticleNode.js
@@ -10,6 +10,7 @@ import PhetColorScheme from '../../../scenery-phet/js/PhetColorScheme.js';
 import Circle from '../../../scenery/js/nodes/Circle.js';
 import Node from '../../../scenery/js/nodes/Node.js';
 import Color from '../../../scenery/js/util/Color.js';
+import RadialGradient from '../../../scenery/js/util/RadialGradient.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import shred from '../shred.js';
 
@@ -43,9 +44,10 @@ class ParticleNode extends Node {
 
     // Create the node a circle with a gradient.
     this.addChild( new Circle( radius, {
-      fill: baseColor,
-      stroke: baseColor.colorUtilsDarker( 0.6 ),
-      lineWidth: 2,
+      fill: new RadialGradient( -radius * 0.4, -radius * 0.4, 0, -radius * 0.4, -radius * 0.4, radius * 1.6 )
+        .addColorStop( 0, baseColor.colorUtilsBrighter( 0.5 ) )
+        .addColorStop( 1, baseColor.colorUtilsDarker( 0.5 ) ),
+      stroke: baseColor.darkerColor( 0.5 ),
       cursor: 'pointer'
     } ) );
   }
