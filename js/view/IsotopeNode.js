f873a0e3656f87d7a4d834b3e8f95176f93c8448
phet-dev <phetadmin@gmail.com>
2021-05-25 16:07:01 -0600

    update copyright dates from daily grunt work

--- js/view/IsotopeNode.js
+++ js/view/IsotopeNode.js
@@ -1,4 +1,4 @@
-// Copyright 2015-2020, University of Colorado Boulder
+// Copyright 2015-2021, University of Colorado Boulder
 
 /**
  * Particle, represented as a circle with a gradient.  This type does not
