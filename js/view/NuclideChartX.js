507e313489c65699ca0395999cde444c4ddf55b3
chrisklus <christopher.klusendorf@colorado.edu>
2021-07-13 12:05:57 -0400

    Revert "Run grunt update-copyright-dates, see https://github.com/phetsims/chipper/issues/1051"
    This reverts commit 1b8bc26299f15ca0e1902f50234cfd74f5004887.

--- js/view/NuclideChartX.js
+++ js/view/NuclideChartX.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2015-2020, University of Colorado Boulder
 
 /**
  * X shape node that represents a non-existant nuclide cell.
--- js/view/ParticleAtomDisplay.js
+++ js/view/ParticleAtomDisplay.js
@@ -1,4 +1,4 @@
-// Copyright 2021, University of Colorado Boulder
+// Copyright 2014-2020, University of Colorado Boulder
 
 /**
  * A node that presents a representation of the atom. Uses ParticleNode's instead of the regular atom
