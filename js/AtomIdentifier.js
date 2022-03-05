78af75507ad7663ba572e85e198a1f87d89769a3
phet-dev <phetadmin@gmail.com>
2022-03-05 03:29:22 -0700

    update copyright dates from daily grunt work

--- js/AtomIdentifier.js
+++ js/AtomIdentifier.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * AtomIdentifier is an object that can be used to identify various things about an atom given its configuration, such
--- js/view/AtomNode.js
+++ js/view/AtomNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * View representation of the atom. Mostly, this is responsible for displaying and updating the labels, since the atom
