d65feec6941dbcdcd6e1b2aaef65d291d6bacaed
phet-dev <phetadmin@gmail.com>
2022-05-24 04:00:05 -0600

    update copyright dates from daily grunt work

--- js/view/ElectronShellView.js
+++ js/view/ElectronShellView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Node that represents the electron shells, aka "orbits", in the view.
