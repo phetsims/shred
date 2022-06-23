f51362abbc294453e63bb5b0ac41f8d3ebe1daf6
jbphet <jpblanco@yahoo.com>
2022-06-23 16:26:43 -0600

    removed code related to highlighting of the nucleus, see https://github.com/phetsims/shred/issues/35

--- js/view/ElectronShellView.js
+++ js/view/ElectronShellView.js
@@ -6,19 +6,14 @@
  * @author John Blanco
  */
 
-import Property from '../../../axon/js/Property.js';
+import Vector2 from '../../../dot/js/Vector2.js';
 import merge from '../../../phet-core/js/merge.js';
-import { KeyboardUtils } from '../../../scenery/js/imports.js';
-import { Circle } from '../../../scenery/js/imports.js';
-import { Node } from '../../../scenery/js/imports.js';
+import { Circle, Node } from '../../../scenery/js/imports.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import shred from '../shred.js';
 
 // constants
 const LINE_DASH = [ 4, 5 ];
-
-// options for the focus highlight, these will be cycled through with the arrow keys
-const FOCUS_HIGHLIGHTS = [ 'CENTER_OPTION', 'INNER_RING', 'OUTER_RING' ];
 
 class ElectronShellView extends Node {
 
@@ -49,7 +44,7 @@ class ElectronShellView extends Node {
       stroke: 'blue',
       lineWidth: 1.5,
       lineDash: LINE_DASH,
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } ),
+      translation: modelViewTransform.modelToViewPosition( Vector2.ZERO ),
       pickable: false,
       tandem: options.tandem.createTandem( 'outerRing' ),
 
@@ -58,12 +53,13 @@ class ElectronShellView extends Node {
       ariaRole: 'option',
       innerContent: 'Outer Electron Ring'
     } );
+    this.addChild( outerRing );
 
     const innerRing = new Circle( modelViewTransform.modelToViewDeltaX( atom.innerElectronShellRadius ), {
       stroke: 'blue',
       lineWidth: 1.5,
       lineDash: LINE_DASH,
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } ),
+      translation: modelViewTransform.modelToViewPosition( Vector2.ZERO ),
       pickable: false,
       tandem: options.tandem.createTandem( 'innerRing' ),
 
@@ -72,96 +68,10 @@ class ElectronShellView extends Node {
       ariaRole: 'option',
       innerContent: 'Inner Electron Ring'
     } );
-
-    // pdom - an invisible node that allows the nucleus to be highlighted.
-    const centerOption = new Node( {
-
-      // pdom
-      tagName: 'div',
-      ariaRole: 'option',
-      innerContent: 'Nucleus'
-    } );
-
-    // pdom - to focus around the actual nucleus, will change in size when the particles in the nucleus change
-    const nucleusFocusHighlight = new Circle( atom.nucleusRadius, {
-      lineWidth: 2,
-      stroke: 'red',
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } )
-    } );
-
-    // pdom - to focus around the outer shell
-    const electronOuterFocusHighlight = new Circle( atom.outerElectronShellRadius, {
-      lineWidth: 2,
-      stroke: 'red',
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } )
-    } );
-
-    // pdom - to focus around the inner shell
-    const electronInnerFocusHighlight = new Circle( atom.innerElectronShellRadius, {
-      lineWidth: 2,
-      stroke: 'red',
-      translation: modelViewTransform.modelToViewPosition( { x: 0, y: 0 } )
-    } );
-
-    const selectValueProperty = new Property( 'none' );
-
-    // Link the property's value to change the focus highlight outlining the different particle placement possibilities.
-    selectValueProperty.lazyLink( newValue => {
-      switch( newValue ) {
-        case ( FOCUS_HIGHLIGHTS[ 0 ] ):
-          this.setFocusHighlight( electronOuterFocusHighlight );
-          break;
-        case ( FOCUS_HIGHLIGHTS[ 1 ] ):
-          this.setFocusHighlight( electronInnerFocusHighlight );
-          break;
-        case ( FOCUS_HIGHLIGHTS[ 2 ] ):
-          this.setFocusHighlight( nucleusFocusHighlight );
-          break;
-        default:
-          throw new Error( 'You tried to set the selectValueProperty to an unsupported value.' );
-      }
-    } );
-
-    // pdom - set the selectProperty when the arrow keys change the html select menu's value.
-    const optionNodes = [ centerOption, innerRing, outerRing ];
-    let currentIndex = 0;
-    const keyListener = {
-      keydown: event => {
-        const key = KeyboardUtils.getEventCode( event.domEvent );
-
-        if ( key === KeyboardUtils.KEY_DOWN_ARROW || key === KeyboardUtils.KEY_RIGHT_ARROW ) {
-          currentIndex = ( currentIndex + 1 ) % optionNodes.length;
-        }
-        else if ( key === KeyboardUtils.KEY_UP_ARROW || key === KeyboardUtils.KEY_LEFT_ARROW ) {
-          currentIndex = currentIndex - 1;
-          if ( currentIndex < 0 ) { currentIndex = optionNodes.length - 1; }
-        }
-
-        // TODO: The requested design for a11y was to use the aria-activedescendant attribute to update the
-        // active node without changing focus. As of this writing, that isn't supported by scenery, but may be in the
-        // future.  When it is, this should be updated.  See https://github.com/phetsims/shred/issues/26.
-        // this.setPDOMAttribute( 'aria-activedescendant', nextElementId );
-        const nextElementId = FOCUS_HIGHLIGHTS[ currentIndex ];
-        selectValueProperty.set( nextElementId );
-      }
-    };
-    this.addInputListener( keyListener );
-
-    // add each node to the view
-    optionNodes.forEach( node => this.addChild( node ) );
-
-    // whenever a nucleon is added or removed, update the configuration of the nucleus and the highlight radius
-    // TODO: !!!WARNING!!! This used to be a multilink with the protonCountProperty and neutronCountProperty, is this okay
-    //  across all usages of ParticleAtom and AtomNode? See https://github.com/phetsims/shred/issues/34.
-    atom.massNumberProperty.link( () => {
-      atom.reconfigureNucleus();
-      const radiusOffset = atom.nucleusRadius === 0 ? 0 : 4;
-      nucleusFocusHighlight.radius = atom.nucleusRadius + radiusOffset;
-    } );
+    this.addChild( innerRing );
 
     // @private called by dispose
     this.disposeElectronShellView = () => {
-      this.removeInputListener( keyListener );
       outerRing.dispose();
       innerRing.dispose();
     };
