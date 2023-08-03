5f3b89ad7881c1826726284ff6e008784b27c2a5
phet-dev <phetadmin@gmail.com>
2023-08-03 03:36:17 -0600

    update copyright dates from daily grunt work

--- js/view/ParticleCountDisplay.ts
+++ js/view/ParticleCountDisplay.ts
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * A node that presents a graphical representation of an atom's configuration. It looks somewhat like a bar graph that
