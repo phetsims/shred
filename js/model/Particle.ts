76e22d4925401076224e20ea7342c741cf12d8bf
jbphet <jpblanco@yahoo.com>
2023-08-25 13:41:15 -0600

    add review comment, see https://github.com/phetsims/build-a-nucleus/issues/165

--- js/model/Particle.ts
+++ js/model/Particle.ts
@@ -1,7 +1,7 @@
 // Copyright 2014-2023, University of Colorado Boulder
 
 /**
- * Model representation of a particle.
+ * Model representation of a subatomic particle, such as a proton.
  *
  * @author John Blanco
  */
@@ -54,6 +54,7 @@ class Particle extends PhetioObject {
   // IDs needed for map-like lookup
   public readonly id = nextParticleId++;
 
+  // REVIEW: Please add a GitHub issue for this to-do or remove it.  See https://github.com/phetsims/shred/commit/b3296309501e2ce7a375cd97baa849b83827b3e0.
   // TODO: investigate getting rid of this and using userControlledProperty
   // Fires when the user stops dragging a particle.
   public readonly dragEndedEmitter = new Emitter<[ Particle ]>( { parameters: [ { valueType: Particle } ] } );
