b66ecfd406458b04f645889be3579e784599c4af
Sam Reid <reids@colorado.edu>
2025-06-24 13:11:34 -0600

    grunt modulify, see https://github.com/phetsims/chipper/issues/1602

--- js/ShredFluent.ts
+++ js/ShredFluent.ts
@@ -10,6 +10,7 @@ import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
 import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
+import FluentComment from '../../chipper/js/browser/FluentComment.js';
 import shred from './shred.js';
 import ShredStrings from './ShredStrings.js';
 
