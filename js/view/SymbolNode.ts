4adecbcd3bda53d9b85ed2e04427cf0685985a52
jbphet <jpblanco@yahoo.com>
2025-12-08 15:20:13 -0700

    add charge notation support for non-interactive symbol, see https://github.com/phetsims/build-an-atom/issues/434

--- js/view/SymbolNode.ts
+++ js/view/SymbolNode.ts
@@ -1,15 +1,18 @@
 // Copyright 2022-2025, University of Colorado Boulder
 
 /**
- * Node that represents an atomic symbol, meaning that it shows the symbol text, the proton count, and the
- * atomic number. It also optionally shows the charge.
+ * Node that represents an atomic symbol, meaning that it shows the symbol text, the proton count, and the atomic
+ * number. It also optionally shows the charge.
  *
  * @author John Blanco (PhET Interactive Simulations)
  * @author Luisa Vargas
  */
 
+import Multilink, { UnknownMultilink } from '../../../axon/js/Multilink.js';
+import Property from '../../../axon/js/Property.js';
 import { TReadOnlyProperty } from '../../../axon/js/TReadOnlyProperty.js';
 import Vector2 from '../../../dot/js/Vector2.js';
+import affirm from '../../../perennial-alias/js/browser-and-node/affirm.js';
 import optionize from '../../../phet-core/js/optionize.js';
 import MathSymbols from '../../../scenery-phet/js/MathSymbols.js';
 import PhetFont from '../../../scenery-phet/js/PhetFont.js';
@@ -17,13 +20,19 @@ import Node, { NodeOptions } from '../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../scenery/js/nodes/Rectangle.js';
 import Text from '../../../scenery/js/nodes/Text.js';
 import AtomIdentifier from '../AtomIdentifier.js';
+import { ChargeNotation } from '../model/ChargeNotation.js';
 import shred from '../shred.js';
 import ShredColors from '../ShredColors.js';
 import ShredConstants from '../ShredConstants.js';
 
 // types
 type SelfOptions = {
+
+  // controls whether charge is displayed and defines the value if so; if null or undefined, charge is not displayed
   chargeProperty?: TReadOnlyProperty<number> | null;
+
+  // controls how charge is displayed, if chargeProperty is provided
+  chargeNotationProperty?: TReadOnlyProperty<ChargeNotation> | null;
 };
 export type SymbolNodeOptions = SelfOptions & NodeOptions;
 
@@ -38,19 +47,20 @@ class SymbolNode extends Node {
   protected readonly chargeDisplay: Text | undefined;
   protected readonly boundingBox: Rectangle;
   protected readonly symbolText: Text;
+  private disposeSymbolNode: () => void;
 
   public constructor( protonCountProperty: TReadOnlyProperty<number>,
                       massNumberProperty: TReadOnlyProperty<number>,
                       providedOptions?: SymbolNodeOptions ) {
 
     const options = optionize<SymbolNodeOptions, SelfOptions, NodeOptions>()( {
-      chargeProperty: null
+      chargeProperty: null,
+      chargeNotationProperty: null
     }, providedOptions );
 
     super( options );
 
-    // Add the bounding box, which is also the root node for everything else
-    // that comprises this node.
+    // Add the bounding box, which is also the root node for everything else that comprises this node.
     this.boundingBox = new Rectangle( 0, 0, SYMBOL_BOX_WIDTH, SYMBOL_BOX_HEIGHT, 0, 0, {
       stroke: 'black',
       lineWidth: 2,
@@ -67,10 +77,11 @@ class SymbolNode extends Node {
 
     // Add the listener to update the symbol text.
     const textCenter = new Vector2( SYMBOL_BOX_WIDTH / 2, SYMBOL_BOX_HEIGHT / 2 );
-    protonCountProperty.link( protonCount => {
+    const updateSymbolText = ( protonCount: number ) => {
       const symbol = AtomIdentifier.getSymbol( protonCount );
       this.symbolText.string = protonCount > 0 ? symbol : '-';
-    } );
+    };
+    protonCountProperty.link( updateSymbolText );
 
     // Don't center in the above listener, in case subtypes want to change the symbol.
     this.symbolText.stringProperty.link( () => {
@@ -86,11 +97,12 @@ class SymbolNode extends Node {
     this.boundingBox.addChild( protonCountDisplay );
 
     // Add the listener to update the proton count.
-    protonCountProperty.link( protonCount => {
-      protonCountDisplay.string = protonCount;
+    const updateProtonNumber = ( protonCount: number ) => {
+      protonCountDisplay.string = protonCount.toString();
       protonCountDisplay.left = NUMBER_INSET;
       protonCountDisplay.bottom = SYMBOL_BOX_HEIGHT - NUMBER_INSET;
-    } );
+    };
+    protonCountProperty.link( updateProtonNumber );
 
     // Add the mass number display.
     this.massNumberDisplay = new Text( '0', {
@@ -100,12 +112,15 @@ class SymbolNode extends Node {
     this.boundingBox.addChild( this.massNumberDisplay );
 
     // Add the listener to update the mass number.
-    massNumberProperty.link( massNumber => {
-      this.massNumberDisplay.string = massNumber;
+    const updateMassNumber = ( massNumber: number ) => {
+      this.massNumberDisplay.string = massNumber.toString();
       this.massNumberDisplay.left = NUMBER_INSET;
       this.massNumberDisplay.top = NUMBER_INSET;
-    } );
+    };
+    massNumberProperty.link( updateMassNumber );
 
+    // If the options dictate, add the charge display.
+    let updateChargeDisplay: null | UnknownMultilink = null;
     if ( options.chargeProperty ) {
 
       // Add the charge display.
@@ -115,16 +130,45 @@ class SymbolNode extends Node {
       } );
       this.boundingBox.addChild( this.chargeDisplay );
 
-      // Add the listener to update the charge.
-      options.chargeProperty.link( charge => {
-        const chargeSign = charge > 0 ? MathSymbols.PLUS : charge < 0 ? MathSymbols.MINUS : '';
-        this.chargeDisplay!.string = `${Math.abs( charge )}${chargeSign}`;
-        this.chargeDisplay!.fill = ShredConstants.CHARGE_TEXT_COLOR( charge );
-        this.chargeDisplay!.right = SYMBOL_BOX_WIDTH - NUMBER_INSET;
-        this.chargeDisplay!.top = NUMBER_INSET;
-      } );
+      // Make sure that we have a value for chargeNotationsProperty.
+      const chargeNotationProperty = options.chargeNotationProperty || new Property<ChargeNotation>( 'signLast' );
 
+      // Add the listener to update the charge.
+      updateChargeDisplay = Multilink.multilink(
+        [ options.chargeProperty, chargeNotationProperty ],
+        ( charge, chargeNotation ) => {
+          affirm( this.chargeDisplay, 'this.chargeDisplay should be defined here' );
+          const chargeSign = charge > 0 ? MathSymbols.PLUS : charge < 0 ? MathSymbols.MINUS : '';
+          this.chargeDisplay.string = chargeNotation === 'signFirst' ?
+                                      `${chargeSign}${Math.abs( charge )}` :
+                                      `${Math.abs( charge )}${chargeSign}`;
+          this.chargeDisplay.fill = ShredConstants.CHARGE_TEXT_COLOR( charge );
+          this.chargeDisplay.right = SYMBOL_BOX_WIDTH - NUMBER_INSET;
+          this.chargeDisplay.top = NUMBER_INSET;
+        }
+      );
     }
+    else {
+
+      // Verify that chargeNotationsProperty was not provided if chargeProperty was not provided.
+      affirm(
+        options.chargeNotationProperty === null,
+        'chargeNotationsProperty should not be provided if chargeProperty is not provided'
+      );
+    }
+
+    this.disposeSymbolNode = () => {
+      protonCountProperty.unlink( updateSymbolText );
+      protonCountProperty.unlink( updateProtonNumber );
+      massNumberProperty.unlink( updateMassNumber );
+      if ( updateChargeDisplay ) {
+        Multilink.unmultilink( updateChargeDisplay );
+      }
+    };
+  }
+
+  public override dispose(): void {
+    super.dispose();
   }
 }
 
