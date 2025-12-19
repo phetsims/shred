ff2f590f2cf43e4fe79f763cee793cd76e5c4b18
Jesse <jesse.greenberg@colorado.edu>
2025-12-19 15:54:38 -0500

    Run modulify after change for https://github.com/phetsims/chipper/issues/1658

--- js/ShredFluent.ts
+++ js/ShredFluent.ts
@@ -10,6 +10,7 @@ import type { FluentVariable } from '../../chipper/js/browser/FluentPattern.js';
 import FluentPattern from '../../chipper/js/browser/FluentPattern.js';
 import FluentConstant from '../../chipper/js/browser/FluentConstant.js';
 import FluentContainer from '../../chipper/js/browser/FluentContainer.js';
+import FluentLibrary from '../../chipper/js/browser-and-node/FluentLibrary.js';
 import shred from './shred.js';
 import ShredStrings from './ShredStrings.js';
 
@@ -188,7 +189,7 @@ addToMapIfDefined( 'a11y_keyboardHelpDialog_periodicTableNode_selectChemicalSymb
 const createFluentFile = (): string => {
   let ftl = '';
   for (const [key, stringProperty] of fluentKeyToStringPropertyMap.entries()) {
-    ftl += `${key} = ${stringProperty.value.replace('\n','\n ')}\n`;
+    ftl += `${key} = ${FluentLibrary.formatMultilineForFtl( stringProperty.value )}\n`;
   }
   return ftl;
 };
