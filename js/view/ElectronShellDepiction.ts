e9339b62f27bd4b129df525af98279122bbed0a9
phet-dev <phetadmin@gmail.com>
2026-03-21 04:53:20 -0600

    update copyright dates from daily grunt work

--- js/view/ElectronShellDepiction.ts
+++ js/view/ElectronShellDepiction.ts
@@ -1,4 +1,4 @@
-// Copyright 2025, University of Colorado Boulder
+// Copyright 2025-2026, University of Colorado Boulder
 
 /**
  * An enumeration of the possible depictions of electron shells in an atom view.
